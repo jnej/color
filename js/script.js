73a972b3c074305a5a69e36ad507f9ed55fdad49
Amanda Johnson <amandajnej@gmail.com>
2017-06-01 17:12:30 +0100

    Add white button and background option

--- js/script.js
+++ js/script.js
@@ -13,4 +13,9 @@ $(document).ready(function() {
     $("body").removeClass();
     $("body").addClass("red-background");
   });
+
+  $("button#white").click(function()  {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+  });
 });
